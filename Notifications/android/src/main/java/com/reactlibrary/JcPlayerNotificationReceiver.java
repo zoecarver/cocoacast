d957df2954384a9748d3ab925e120689000bcfc9
zoe <z.zoelec2@gmail.com>
2017-11-09 10:33:36 -0800

    Add Notifications

---
+++ Notifications/android/src/main/java/com/reactlibrary/JcPlayerNotificationReceiver.java
@@ -0,0 +1,51 @@
+package com.reactlibrary;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.util.Log;
+
+import com.zmxv.RNSound.RNSoundModule;
+
+public class JcPlayerNotificationReceiver extends BroadcastReceiver {
+    public JcPlayerNotificationReceiver() {
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("SOUND", "recived");
+        String action = "";
+
+        MediaPlayer player = RNSoundModule.playerPool.get(1);
+
+        JcNotificationPlayerService jcNotificationPlayer;
+        jcNotificationPlayer = new JcNotificationPlayerService(context);
+
+        if (intent.hasExtra(com.reactlibrary.JcNotificationPlayerService.ACTION)) {
+            action = intent.getStringExtra(com.reactlibrary.JcNotificationPlayerService.ACTION);
+        }
+
+        switch (action) {
+            case JcNotificationPlayerService.PLAY:
+                try {
+                    com.zmxv.RNSound.RNSoundModule.play_notification(1, null);
+                    jcNotificationPlayer.createNotificationPlayer("hello", 1);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case JcNotificationPlayerService.PAUSE:
+                try {
+                    if(player != null) {
+                        com.zmxv.RNSound.RNSoundModule.pause_notification(1);
+                        jcNotificationPlayer.createNotificationPlayer("hello", 1);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+}
